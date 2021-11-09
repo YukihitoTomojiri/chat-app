022220d8263566e28ee900ea3e4c8d6a93ef2838
YukihitoTomojiri <75321573+YukihitoTomojiri@users.noreply.github.com>
2021-11-09 21:54:06 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user! #authenticate_user!は処理が呼ばれた段階でユーザーがログインしていなければ、ログイン画面に遷移させるメソッド
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
